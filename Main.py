f5ca1f5a863cfe0c2877c83a5845973d9fcd163e
DiGU <103802166+BlitzSlayerYT@users.noreply.github.com>
2023-05-14 13:03:30 +0800

    First commit
    This is the first commit to the Calculator App I made in python.
    Added Main.py file
    Added README.md file
    Added .gitignore file

---
+++ Main.py
@@ -0,0 +1,38 @@
+print("==Calculator Console App==")
+print("Made by @CodeSlayer")
+
+while True:
+
+    num1 = int(input("input the first number > "))
+    num2 = int(input("input the second number > "))
+    action = input("Calculation(add,sub,times,divide) > ")
+    print()
+
+    add = num1 + num2
+    subtraction = num1 - num2
+    multiplication = num1 * num2
+    division = num1 / num2
+
+    if action == "add":
+        print("result =", add)
+        print()
+    elif action == "sub":
+        print("result =", subtraction)
+        print()
+    elif action == "times":
+        print("result =", multiplication)
+        print()
+    elif action == "divide":
+        print("result =", division)
+        print()
+    else:
+        print("That operation doesn't exist")
+        print()
+
+    exit = input("Do you want to exit?(Y/N) > ")
+    if exit == "Y":
+        print("exit world")
+        break
+    else:
+        print()
+        continue
